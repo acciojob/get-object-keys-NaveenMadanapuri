1e35b0326c5aceb7bdc36405aeb1406e38f8d6d4
NaveenMadanapuri <139567313+NaveenMadanapuri@users.noreply.github.com>
2023-12-28 19:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const student = {
+  name: "John Doe"
+};
+
+// Add a property to the Object prototype
+Object.prototype.getKeys = function () {
+  // Use Object.keys() to get an array of keys
+  return Object.keys(this);
+};
+
+// Call the getKeys() method on the 'student' object
+const keysArray = student.getKeys();
+
+// Log the result
+console.log(keysArray);
